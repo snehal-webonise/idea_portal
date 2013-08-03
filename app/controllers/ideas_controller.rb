14b2c8caad915819627dac19c0f0350073fce274
Unnati Patil <unnati@weboniselab.com>
2013-08-04 03:55:15 +0530

    Add like count at idea list

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,7 +2,7 @@ class IdeasController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
 
   def index
-    @ideas = Idea.paginate(:page => params[:page], :per_page => 2)
+    @ideas = Idea.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
